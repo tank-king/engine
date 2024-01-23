f8044430b1d9005b8e42269ee3f4baa9c20cf5f3
tank-king <tankking9833@gmail.com>
2024-01-23 08:59:35 +0530

    added BaseStructure(...)

--- base.py
+++ base.py
@@ -8,5 +8,5 @@ class BaseStructure:
     def update(self, events: list[pygame.event.Event], dt: float = 1.0):
         pass
 
-    def draw(self):
+    def draw(self, pos: Point, scale: float = 1.0, angle: float = 0.0):
         pass
